e475ede6058afa55ae53bb235e77313d82e5a5fd
wildan <wildanbudi19@gmail.com>
2022-06-11 20:51:58 +0700

    Initial Commit

---
+++ Mini_Challenge_2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mini_Challenge_2
+//
+//  Created by Wildan Budi on 11/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
