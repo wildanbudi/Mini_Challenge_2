798faad18d1ee70dbb502b16cd62e7b9a511d3f9
finnchristoffer <finn.christoffer@gmail.com>
2022-06-25 23:33:07 +0700

    update connect to tableview + tableview in detail + vegeResto type hidden or not hidden

--- Mini_Challenge_2/Pages/Detail_Restaurant/Controller/DetailViewController.swift
+++ Mini_Challenge_2/Pages/Detail_Restaurant/Controller/DetailViewController.swift
@@ -7,13 +7,16 @@
 
 import UIKit
 
-class DetailViewController: UIViewController {
+class DetailViewController: UIViewController,UITableViewDelegate {
     
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     var restaurantData: [Restaurants]!
     var menuRestaurantData: [Menus]!
     var menus: [Menus]!
     var restaurantDetail: Restaurants!
+    var reviewsData: [Reviews]!
     
+    @IBOutlet weak var DetailReviewTableView: UITableView!
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var locationButton: UIButton!
     @IBOutlet weak var RestaurantLabel: UILabel!
@@ -26,8 +29,6 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var MapsImageButton: UIButton!
     @IBOutlet weak var PriceTagMax: UILabel!
     
-    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -39,10 +40,15 @@ class DetailViewController: UIViewController {
         collectionView.collectionViewLayout = createLayout()
         
         getAllRestaurant()
+        
+        DetailReviewTableView.delegate = self
+        DetailReviewTableView.dataSource = self
+        
         let restaurant = restaurantData.filter({(r: Restaurants) -> Bool in
-            return r.name == "Che En Vegetarian"
+            return r.name == "Formaggio Coffee and Resto"
         }).first ?? Restaurants(context: context)
         menuRestaurantData = (restaurant.menus!.allObjects as! [Menus])
+        reviewsData = (restaurant.reviews!.allObjects as! [Reviews])
         
         // Data Restaurant from protocol
         restaurantDetail = restaurant
@@ -57,7 +63,6 @@ class DetailViewController: UIViewController {
     
     // Navigation Bar Atribute 
     private func configureBarAtribute() {
-        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Back", style: .done, target: self, action: nil)
         navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "heart"), style: .done, target: self, action: nil)
     }
     
@@ -68,6 +73,7 @@ class DetailViewController: UIViewController {
             restaurantData = restaurants
             DispatchQueue.main.async {
                 self.collectionView.reloadData()
+                self.DetailReviewTableView.reloadData()
             }
         } catch _ {
             
@@ -78,7 +84,12 @@ class DetailViewController: UIViewController {
     func connectControllerToData () {
         let imgMap = UIImage(data: restaurantDetail.imageMap! )
         RestaurantLabel.text = restaurantDetail.name
-        RestaurantType.image = UIImage(data: restaurantDetail.image!)
+        if restaurantDetail.vegeResto == false {
+            RestaurantType.isHidden = true
+        }else{
+            RestaurantType.isHidden = false
+        }
+        //RestaurantType.image = UIImage(data: restaurantDetail.image!)
         PriceTagMin.text = restaurantDetail.priceMin
         PriceTagMax.text = restaurantDetail.priceMax
         RateTag.text = String(restaurantDetail.rating)
@@ -90,6 +101,7 @@ class DetailViewController: UIViewController {
         MapsImageButton.setImage(imgMap, for: .normal)
         MapsImageButton.layer.cornerRadius = 15
         MapsImageButton.clipsToBounds = true
+        MapsImageButton.imageView?.layer.transform = CATransform3DMakeScale(0.5, 0.5, 0.5)
     }
     
     // Map Direction Function
@@ -116,9 +128,16 @@ class DetailViewController: UIViewController {
     @IBAction func RatePressed(_ sender: UIButton) {
         performSegue(withIdentifier: "goToRate", sender: self)
     }
+    
+    @IBAction func ReviewAll(_ sender: UIButton) {
+        performSegue(withIdentifier: "goToReviewAll", sender: self)
+    }
+    
 }
 
-extension DetailViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+
+
+extension DetailViewController: UICollectionViewDelegate, UICollectionViewDataSource,UITableViewDataSource {
     
 //    func numberOfSections(in collectionView: UICollectionView) -> Int {
 //        return sections.count
@@ -134,8 +153,57 @@ extension DetailViewController: UICollectionViewDelegate, UICollectionViewDataSo
         cell.cellImageView.image = UIImage(data: menu.image!)
         return cell
     }
+        func numberOfSections(in tableView: UITableView) -> Int {
+            return 1
+        }
+        
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return reviewsData.count
+        }
+        
+        func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+            return 172
+        }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let DetailReviewCell = tableView.dequeueReusableCell(withIdentifier: "DetailReviewCell", for: indexPath) as! DetailReviewTableViewCell
+            
+            let review = reviewsData[indexPath.row]
+            DetailReviewCell.DetailUserImageView.image = UIImage(data: (review.user?.image)!)
+            DetailReviewCell.DetailUserImageView.layer.cornerRadius = DetailReviewCell.DetailUserImageView.frame.width / 2;
+            DetailReviewCell.DetailUserImageView.layer.masksToBounds = true
+            DetailReviewCell.DetailUserImageView.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
+            DetailReviewCell.DetailUserImageView.setContentCompressionResistancePriority(.defaultLow, for: .vertical)
+            DetailReviewCell.DetailUserNameLabel.text = review.user?.name
+            DetailReviewCell.DetailUserCommentLabel.text = review.comment
+            let formatter = DateFormatter()
+            formatter.dateFormat = "dd-MM-yyyy"
+            let formattedDate = formatter.string(from: review.date!)
+            
+            DetailReviewCell.DetailReviewDataLabel.text = formattedDate
+            if review.image != nil {
+                DetailReviewCell.DetailReviewImageView.image = UIImage(data: review.image!)
+                DetailReviewCell.DetailReviewImageView.layer.cornerRadius = 7
+            } else {
+                DetailReviewCell.DetailReviewImageView.isHidden = true
+            }
+            
+            let userRates: [UIImageView] = [
+                DetailReviewCell.DetailUserRateIcon1, DetailReviewCell.DetailUserRateIcon2, DetailReviewCell.DetailUserRateIcon3, DetailReviewCell.DetailUserRateIcon4, DetailReviewCell.DetailUserRateIcon5
+            ]
+            let rateCount = 1...review.rate
+            for n in rateCount {
+                userRates[Int(n) - 1].image  = UIImage(systemName: "star.fill")
+                userRates[Int(n) - 1].tintColor = UIColor(red: 255/255, green: 199/255, blue: 0/255, alpha: 1)
+            }
+            
+            return DetailReviewCell
+        }
+    
+
     
     @objc func askToOpenMap() {
-        OpenMapDirections.present(in: self, sourceView: locationButton)
+        //OpenMapDirections.present(in: self, sourceView: sender)
     }
 }
+
